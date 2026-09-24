3eb42b66eefad6514e7af98bf4729fd2add5b968
Jeevan M <jeevanjeeva0304@gmail.com>
2026-09-24 12:13:21 +0530

    Configure Firebase for CampusHive frontend

---
+++ frontend/campushive_app/lib/firebase_options.dart
@@ -0,0 +1,18 @@
+// File generated from the official Firebase Android client configuration.
+
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+/// Default Firebase options for CampusHive Android.
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return android;
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyBFU44MEkq_UhQiB2V_Xf5bqp4sFElOx2c',
+    appId: '1:318085072290:android:78a802cdd870c487eb9efb',
+    messagingSenderId: '318085072290',
+    projectId: 'campushive-61b45',
+    storageBucket: 'campushive-61b45.firebasestorage.app',
+  );
+}
